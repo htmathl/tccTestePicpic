d5aa54f90121191673b92bdcf116bd6d82a11c8d
Anna <annabserikyaku@gmail.com>
2020-06-07 22:07:31 -0300

    criar e alterar primeira versão do overview

--- app/src/main/java/com/example/testepicpic/MainActivity.java
+++ app/src/main/java/com/example/testepicpic/MainActivity.java
@@ -4,11 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    LineChart graficoGlicemia;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        graficoGlicemia = (LineChart) findViewById(R.id.grafico_glicemia);
+
+
     }
 }
