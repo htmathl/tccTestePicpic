cb2c53a94e6090156681349c7052b7e14e34a7db
Anna <annabserikyaku@gmail.com>
2020-06-15 22:13:50 -0300

    Padronizando btn e edt do cadastro

---
+++ app/src/main/java/com/example/testepicpic/tela_cadastro2.java
@@ -0,0 +1,13 @@
+package com.example.testepicpic;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class tela_cadastro2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_cadastro2);
+    }
+}
