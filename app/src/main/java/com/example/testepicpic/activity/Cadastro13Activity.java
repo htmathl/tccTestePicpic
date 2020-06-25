adfb241f3f9fc5db0b7480aaee00a397c8a19e01
Anna <annabserikyaku@gmail.com>
2020-06-24 21:36:59 -0300

    refazendo a tela 13

---
+++ app/src/main/java/com/example/testepicpic/activity/Cadastro13Activity.java
@@ -0,0 +1,16 @@
+package com.example.testepicpic.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.testepicpic.R;
+
+public class Cadastro13Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_13);
+    }
+}
