9126d83086b8d9b0c1234fd9e437e50f30a62bf7
matheus <matheus.domingues3@etec.sp.gov.br>
2020-10-11 21:16:27 -0300

    LUISA PEDIDA EN CASAMENTO

--- app/src/main/java/com/example/testepicpic/activity/SliderActivity.java
+++ app/src/main/java/com/example/testepicpic/activity/SliderActivity.java
@@ -1,73 +1,13 @@
 package com.example.testepicpic.activity;
 
-import android.content.Intent;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.view.WindowManager;
 
-import com.example.testepicpic.R;
-import com.example.testepicpic.config.ConfigFirebase;
-import com.google.firebase.auth.FirebaseAuth;
-import com.heinrichreimersoftware.materialintro.app.IntroActivity;
-import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
-
-public class SliderActivity extends IntroActivity {
-
-    private FirebaseAuth autenticacao;
+public class SliderActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        setFullscreen(false);
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_slider);
-
-        setButtonBackVisible(false);
-        setButtonNextVisible(false);
-
-        addSlide(new FragmentSlide.Builder()
-                .background(R.color.colorSlider)
-                .fragment(R.layout.fragment_intro_1)
-                .build()
-        );
-
-        addSlide(new FragmentSlide.Builder()
-                .background(R.color.colorSlider)
-                .fragment(R.layout.fragment_intro_2)
-                .build()
-        );
-
-        addSlide(new FragmentSlide.Builder()
-                .background(R.color.colorSlider)
-                .fragment(R.layout.fragment_intro_3)
-                .build()
-        );
-
-        addSlide(new FragmentSlide.Builder()
-                .background(R.color.colorSlider)
-                .fragment(R.layout.fragment_intro_4)
-                .canGoForward(false)
-                .build()
-        );
-
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        verificarUserLogado();
-    }
-
-    public void btnEntrar(View v) {
-        startActivity(new Intent(this, PrincipalActivity.class));
-    }
-    public void verificarUserLogado() {
-        autenticacao = ConfigFirebase.getFirebaseAutenticacao();
-        if(autenticacao.getCurrentUser() != null) {
-            abrirTelaPrincipal();
-        }
-    }
-    public void abrirTelaPrincipal() {
-        startActivity(new Intent(this, MainActivity.class));
-        finish();
+        setContentView(R.layout.activity_slider);
     }
 }
