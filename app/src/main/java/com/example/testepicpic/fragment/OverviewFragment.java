4b94c20f8f6ce87b91f5ca9c01021e1d12aaddb6
matheus <matheus.domingues3@etec.sp.gov.br>
2020-10-30 13:42:41 -0300

    terminar super gambiarra dos copos

--- app/src/main/java/com/example/testepicpic/fragment/OverviewFragment.java
+++ app/src/main/java/com/example/testepicpic/fragment/OverviewFragment.java
@@ -5,9 +5,11 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 
+import android.provider.ContactsContract;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -24,10 +26,15 @@ import com.example.testepicpic.activity.*;
 import com.example.testepicpic.broadcast.DayChangedBroadcastReceiver;
 import com.example.testepicpic.config.*;
 import com.example.testepicpic.helper.Base64Custom;
+import com.example.testepicpic.model.Usuario;
 import com.example.testepicpic.service.MesmoDia;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -102,7 +109,14 @@ public class OverviewFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_main_overview, container, false);
 
+        ref = ConfigFirebase.getFirebase();
+
+        pYear = Calendar.getInstance().get(Calendar.YEAR);
+        pMonth = (Calendar.getInstance().get(Calendar.MONTH)+1);
+        pDay = Calendar.getInstance().get(Calendar.DAY_OF_MONTH);
+
         Button btnSairProv = view.findViewById(R.id.btnExames);
+
         copo1 = view.findViewById(R.id.copo1);
         copo2 = view.findViewById(R.id.copo2);
         copo3 = view.findViewById(R.id.copo3);
@@ -128,24 +142,155 @@ public class OverviewFragment extends Fragment {
         final CheckBox[] listaCopos = {copo1, copo2, copo3, copo4, copo5, copo6, copo7, copo8, copo9, copo10, copo11,
                 copo12, copo13, copo14, copo15, copo16};
 
+        recuperarUsuario();
+
+        try {
+
+            final DatabaseReference reference = ref.child("users")
+                    .child(currentId)
+                    .child("inserção")
+                    .child("bem-estar")
+                    .child(String.valueOf(pYear))
+                    .child(String.valueOf(pMonth))
+                    .child(String.valueOf(pDay))
+                    .child("Água");
+
+            reference.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                    String agua = snapshot.getValue().toString();
+
+                    switch (agua) {
+
+                        case "0":
+                            for(CheckBox copo : listaCopos)
+                                copo.setChecked(false);
+                            txtQntd.setText(0 + " ml");
+                            tbOutrosCopos.setVisibility(View.GONE);
+                            break;
+                        case "1":
+                            copo1.setChecked(true);
+                            copo2.setChecked(true);
+                            txtQntd.setText(250 + " ml");
+                            tbOutrosCopos.setVisibility(View.GONE);
+                            break;
+                        case "2":
+                            for(int i = 0; i < 2; i++)
+                                listaCopos[i].setChecked(true);
+                            txtQntd.setText(500 + " ml");
+                            tbOutrosCopos.setVisibility(View.GONE);
+                            break;
+                        case "3":
+                            for(int i = 0; i < 3; i++)
+                                listaCopos[i].setChecked(true);
+                            txtQntd.setText(750 + " ml");
+                            tbOutrosCopos.setVisibility(View.GONE);
+                            break;
+                        case "4":
+                            for(int i = 0; i < 4; i++)
+                                listaCopos[i].setChecked(true);
+                            txtQntd.setText(1000 + " ml");
+                            tbOutrosCopos.setVisibility(View.GONE);
+                            break;
+                        case "5":
+                            for(int i = 0; i < 5; i++)
+                                listaCopos[i].setChecked(true);
+                            txtQntd.setText(1250 + " ml");
+                            tbOutrosCopos.setVisibility(View.GONE);
+                            break;
+                        case "6":
+                            for(int i = 0; i < 6; i++)
+                                listaCopos[i].setChecked(true);
+                            txtQntd.setText(1500 + " ml");
+                            tbOutrosCopos.setVisibility(View.GONE);
+                            break;
+                        case "7":
+                            for(int i = 0; i < 7; i++)
+                                listaCopos[i].setChecked(true);
+                            txtQntd.setText(1750 + " ml");
+                            tbOutrosCopos.setVisibility(View.GONE);
+                            break;
+                        case "8":
+                            for(int i = 0; i < 8; i++)
+                                listaCopos[i].setChecked(true);
+                            txtQntd.setText(2000 + " ml");
+                            tbOutrosCopos.setVisibility(View.VISIBLE);
+                            break;
+                        case "9":
+                            for(int i = 0; i < 9; i++)
+                                listaCopos[i].setChecked(true);
+                            txtQntd.setText(2250 + " ml");
+                            tbOutrosCopos.setVisibility(View.VISIBLE);
+                            break;
+                        case "10":
+                            for(int i = 0; i < 10; i++)
+                                listaCopos[i].setChecked(true);
+                            txtQntd.setText(2500 + " ml");
+                            tbOutrosCopos.setVisibility(View.VISIBLE);
+                            break;
+                        case "11":
+                            for(int i = 0; i < 11; i++)
+                                listaCopos[i].setChecked(true);
+                            txtQntd.setText(2750 + " ml");
+                            tbOutrosCopos.setVisibility(View.VISIBLE);
+                            break;
+                        case "12":
+                            for(int i = 0; i < 12; i++)
+                                listaCopos[i].setChecked(true);
+                            txtQntd.setText(3000 + " ml");
+                            tbOutrosCopos.setVisibility(View.VISIBLE);
+                            break;
+                        case "13":
+                            for(int i = 0; i < 13; i++)
+                                listaCopos[i].setChecked(true);
+                            txtQntd.setText(3250 + " ml");
+                            tbOutrosCopos.setVisibility(View.VISIBLE);
+                            break;
+                        case "14":
+                            for(int i = 0; i < 14; i++)
+                                listaCopos[i].setChecked(true);
+                            txtQntd.setText(3500 + " ml");
+                            tbOutrosCopos.setVisibility(View.VISIBLE);
+                            break;
+                        case "15":
+                            for(int i = 0; i < 15; i++)
+                                listaCopos[i].setChecked(true);
+                            txtQntd.setText(3750 + " ml");
+                            tbOutrosCopos.setVisibility(View.VISIBLE);
+                            break;
+                        case "16":
+                            for(int i = 0; i < 16; i++)
+                                listaCopos[i].setChecked(true);
+                            txtQntd.setText(4000 + " ml");
+                            tbOutrosCopos.setVisibility(View.VISIBLE);
+                            break;
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }});
+
+        } catch (Exception e) {
+
+            Toast.makeText(getActivity(), e.toString(), Toast.LENGTH_SHORT).show();
+
+        }
+
         if(getActivity() != null) {
             getActivity().startService(new Intent(getActivity(), MesmoDia.class));
             getActivity().registerReceiver(dayChangedBroadcastReceiver, DayChangedBroadcastReceiver.getIntentFilter());
         }
 
-        ref = ConfigFirebase.getFirebase();
-
-        pYear = Calendar.getInstance().get(Calendar.YEAR);
-        pMonth = (Calendar.getInstance().get(Calendar.MONTH)+1);
-        pDay = Calendar.getInstance().get(Calendar.DAY_OF_MONTH);
-
         copo1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                if(!copo1.isChecked()) {
+                recuperarUsuario();
 
-                    recuperarUsuario();
+                if(!copo1.isChecked()) {
 
                    for(CheckBox copo : listaCopos)
                        copo.setChecked(false);
@@ -165,6 +310,7 @@ public class OverviewFragment extends Fragment {
                             .setValue(0);
 
                 } else {
+
                     qntd = 0;
                     qntd = qntd + 250;
                     txtQntd.setText(qntd + " ml");
@@ -225,7 +371,7 @@ public class OverviewFragment extends Fragment {
                             .setValue(2);
                 }
 
-                }
+            }
         });
 
         copo3.setOnClickListener(new View.OnClickListener() {
@@ -864,6 +1010,8 @@ public class OverviewFragment extends Fragment {
                 autenticacao.signOut();
                 startActivity(new Intent(getActivity(), SliderActivity.class));
                 getActivity().finish();
+
+
             }
         });
 
@@ -874,14 +1022,94 @@ public class OverviewFragment extends Fragment {
     public void onStart() {
         super.onStart();
 
-        copo8 = getView().findViewById(R.id.copo8);
-        tbOutrosCopos = getView().findViewById(R.id.tbrOutrosCopos);
+        declarar();
 
-        if(copo8.isChecked()) {
+        if (copo8.isChecked())
             tbOutrosCopos.setVisibility(View.VISIBLE);
-        }
 
-        getActivity().registerReceiver(dayChangedBroadcastReceiver, DayChangedBroadcastReceiver.getIntentFilter());
+        if (getActivity() != null)
+            getActivity().registerReceiver(dayChangedBroadcastReceiver, DayChangedBroadcastReceiver.getIntentFilter());
+
+        if (copo1.isChecked()) {
+            txtQntd.setText(250 + " ml");
+
+            if (copo2.isChecked())
+                txtQntd.setText(500 + " ml");
+            else
+                txtQntd.setText(250 + " ml");
+
+            if (copo3.isChecked())
+                txtQntd.setText(750 + " ml");
+            else
+                txtQntd.setText(500 + " ml");
+
+            if (copo4.isChecked())
+                txtQntd.setText(1000 + " ml");
+            else
+                txtQntd.setText(750 + " ml");
+
+            if (copo5.isChecked())
+                txtQntd.setText(1250 + " ml");
+            else
+                txtQntd.setText(1000 + " ml");
+
+            if (copo6.isChecked())
+                txtQntd.setText(1500 + " ml");
+            else
+                txtQntd.setText(1250 + " ml");
+
+            if (copo7.isChecked())
+                txtQntd.setText(1750 + " ml");
+            else
+                txtQntd.setText(1500 + " ml");
+
+            if (copo8.isChecked())
+                txtQntd.setText(2000 + " ml");
+            else
+                txtQntd.setText(1750 + " ml");
+
+            if (copo9.isChecked())
+                txtQntd.setText(2250 + " ml");
+            else
+                txtQntd.setText(2000 + " ml");
+
+            if (copo10.isChecked())
+                txtQntd.setText(2500 + " ml");
+            else
+                txtQntd.setText(2250 + " ml");
+
+            if (copo11.isChecked())
+                txtQntd.setText(2750 + " ml");
+            else
+                txtQntd.setText(2500 + " ml");
+
+            if (copo12.isChecked())
+                txtQntd.setText(3000 + " ml");
+            else
+                txtQntd.setText(2750 + " ml");
+
+            if (copo13.isChecked())
+                txtQntd.setText(3250 + " ml");
+            else
+                txtQntd.setText(3000 + " ml");
+
+            if (copo14.isChecked())
+                txtQntd.setText(3500 + " ml");
+            else
+                txtQntd.setText(3250 + " ml");
+
+            if (copo15.isChecked())
+                txtQntd.setText(3750 + " ml");
+            else
+                txtQntd.setText(3500 + " ml");
+
+            if (copo16.isChecked())
+                txtQntd.setText(4000 + " ml");
+            else
+                txtQntd.setText(3750 + " ml");
+
+        } else
+            txtQntd.setText(0 + " ml");
 
     }
 
@@ -889,29 +1117,15 @@ public class OverviewFragment extends Fragment {
         @Override
         public void onDayChanged() {
 
-            copo1 = getView().findViewById(R.id.copo1);
-            copo2 = getView().findViewById(R.id.copo2);
-            copo3 = getView().findViewById(R.id.copo3);
-            copo4 = getView().findViewById(R.id.copo4);
-            copo5 = getView().findViewById(R.id.copo5);
-            copo6 = getView().findViewById(R.id.copo6);
-            copo7 = getView().findViewById(R.id.copo7);
-            copo8 = getView().findViewById(R.id.copo8);
-            copo9 = getView().findViewById(R.id.copo9);
-            copo10 = getView().findViewById(R.id.copo10);
-            copo11 = getView().findViewById(R.id.copo11);
-            copo12 = getView().findViewById(R.id.copo12);
-            copo13 = getView().findViewById(R.id.copo13);
-            copo14 = getView().findViewById(R.id.copo14);
-            copo15 = getView().findViewById(R.id.copo15);
-            copo16 = getView().findViewById(R.id.copo16);
+            declarar();
 
             final CheckBox[] listaCopos = {copo1, copo2, copo3, copo4, copo5, copo6, copo7, copo8, copo9, copo10, copo11,
                     copo12, copo13, copo14, copo15, copo16};
 
-            for(CheckBox copo : listaCopos) {
+            for(CheckBox copo : listaCopos)
                 copo.setChecked(false);
-            }
+
+            tbOutrosCopos.setVisibility(View.GONE);
 
         }
     };
@@ -925,8 +1139,29 @@ public class OverviewFragment extends Fragment {
             String email = auth.getCurrentUser().getEmail();
             assert email != null;
             currentId = Base64Custom.codificarBase64(email);
-
         }
+
+    }
+
+    public void declarar() {
+
+        copo1 = getView().findViewById(R.id.copo1);
+        copo2 = getView().findViewById(R.id.copo2);
+        copo3 = getView().findViewById(R.id.copo3);
+        copo4 = getView().findViewById(R.id.copo4);
+        copo5 = getView().findViewById(R.id.copo5);
+        copo6 = getView().findViewById(R.id.copo6);
+        copo7 = getView().findViewById(R.id.copo7);
+        copo8 = getView().findViewById(R.id.copo8);
+        copo9 = getView().findViewById(R.id.copo9);
+        copo10 = getView().findViewById(R.id.copo10);
+        copo11 = getView().findViewById(R.id.copo11);
+        copo12 = getView().findViewById(R.id.copo12);
+        copo13 = getView().findViewById(R.id.copo13);
+        copo14 = getView().findViewById(R.id.copo14);
+        copo15 = getView().findViewById(R.id.copo15);
+        copo16 = getView().findViewById(R.id.copo16);
+        tbOutrosCopos = getView().findViewById(R.id.tbrOutrosCopos);
 
     }
 
