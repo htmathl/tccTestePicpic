8d8d7804ac3e5498ecee45088c40eacb2d5b273b
Joy010 <joycenovaes.01@gmail.com>
2020-11-06 14:54:21 -0300

    tentar perfil, ababou de ficar só o layout

--- app/src/main/java/com/example/testepicpic/fragment/ConfigPerfilFragment.java
+++ app/src/main/java/com/example/testepicpic/fragment/ConfigPerfilFragment.java
@@ -1,39 +1,27 @@
 package com.example.testepicpic.fragment;
 
 import android.Manifest;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.net.Uri;
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.Toast;
+import android.widget.TextView;
 
 import com.example.testepicpic.R;
-import com.example.testepicpic.config.ConfigFirebase;
 import com.example.testepicpic.helper.Permissao;
 import com.example.testepicpic.model.Usuario;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.UploadTask;
-
-import java.io.ByteArrayOutputStream;
 
 import de.hdodenhof.circleimageview.CircleImageView;
-
-import static android.app.Activity.RESULT_OK;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -41,16 +29,18 @@ import static android.app.Activity.RESULT_OK;
  * create an instance of this fragment.
  */
 public class ConfigPerfilFragment extends Fragment {
-    public String[] permissoesNecessarias = new String[] {
-            Manifest.permission.READ_EXTERNAL_STORAGE,
-            Manifest.permission.CAMERA
-    };
-    private ImageButton imgbtnCamera, imgbtnGaleria;
-    private static final int SELECAO_CAMERA = 100;
-    private static final int SELECAO_GALERIA =200;
+
+    private Button btnSalvarPerfil;
+    private TextView txtAlterarFoto;
+    private EditText edtNomeUser;
     private CircleImageView circleImageViewPerfil;
-    private StorageReference storageReference;
-    private String IdentificadorUsuario;
+
+
+
+
+   // private StorageReference storageReference;
+   // private String identificadorUsuario;
+
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -60,8 +50,10 @@ public class ConfigPerfilFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-    private Object View;
-    private StorageReference ConfiguracaoFirebase;
+
+    public ConfigPerfilFragment() {
+        // Required empty public constructor
+    }
 
     /**
      * Use this factory method to create a new instance of
@@ -69,7 +61,7 @@ public class ConfigPerfilFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment PerfilFragment.
+     * @return A new instance of fragment ConfigPerfilFragment.
      */
     // TODO: Rename and change types and number of parameters
     public static ConfigPerfilFragment newInstance(String param1, String param2) {
@@ -79,10 +71,6 @@ public class ConfigPerfilFragment extends Fragment {
         args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
-    }
-
-    public ConfigPerfilFragment() {
-        // Required empty public constructor
     }
 
     @Override
@@ -97,113 +85,14 @@ public class ConfigPerfilFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_config_perfil, container, false);
+        View view = inflater.inflate(R.layout.fragment_config_perfil,container,false);
 
-        imgbtnGaleria = view.findViewById(R.id.imgbtnGaleria);
-        imgbtnCamera = view.findViewById(R.id.imgbtnCamera);
-        circleImageViewPerfil = view.findViewById(R.id.circleImageViewPerfil);
-        storageReference= ConfigFirebase.getFirebaseStorage();
-        IdentificadorUsuario = Usuario.getIdentificadorUsuario();
+        btnSalvarPerfil= view.findViewById(R.id.btnSalvarPerfil);
+        txtAlterarFoto = view.findViewById(R.id.txtAlterarFoto);
+        edtNomeUser= view.findViewById(R.id.edtNomeUser);
 
-        Permissao.validarpermisoes(permissoesNecessarias, getActivity(), 1);
+        return inflater.inflate(R.layout.fragment_config_perfil, container, false);
 
-
-        imgbtnCamera.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent();
-                if (i.resolveActivity(getActivity().getPackageManager()) != null) {
-                    startActivityForResult(i, SELECAO_CAMERA);
-                }
-            }
-        });
-
-        imgbtnGaleria.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(android.view.View view) {
-                Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                if (i.resolveActivity(getActivity().getPackageManager()) != null) {
-                    startActivityForResult(i, SELECAO_GALERIA);
-                }
-            }
-        });
-        return view;
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if( resultCode == RESULT_OK) {
-            Bitmap imagem = null;
-            try {
-                switch (resultCode){
-
-                    case SELECAO_CAMERA:
-                        imagem= (Bitmap) data.getExtras().get("data");
-                        break;
-
-                    case SELECAO_GALERIA:
-                        Uri localImagemSelecionada = data.getData();
-                        imagem= MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(),localImagemSelecionada);
-                        break;
-                }
-                if (imagem != null){
-
-
-                    circleImageViewPerfil.setImageBitmap(imagem);
-                    //recuperar dados da imagem no Firebase
-                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                    imagem.compress(Bitmap.CompressFormat.JPEG,70, baos);
-                    byte[] dadosImagem = baos.toByteArray();
-
-                    //salvando no firebase
-                    StorageReference imagemRef = storageReference.child("fotoperfil")
-                            .child(IdentificadorUsuario+ ".jpeg");
-
-                    UploadTask uploadTask = imagemRef.putBytes(dadosImagem);
-                    uploadTask.addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            Toast.makeText(getActivity(), "Erro ao fazer upload da imagem", Toast.LENGTH_SHORT).show();
-                        }
-                    }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                        @Override
-                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                            Toast.makeText(getActivity(), "Sucesso ao fazer upload da imagem", Toast.LENGTH_SHORT).show();
-                        }
-                    });
-
-                }
-            }
-            catch (Exception e){
-                e.printStackTrace();
-            }
-        }
-    }
-
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
-        for(int permissaoResult: grantResults){
-            if (permissaoResult == PackageManager.PERMISSION_DENIED){
-
-            }
-        }
-    }
-    private void alertaVlidarPermissao(){
-        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
-        builder.setTitle("Permissões Negadas");
-        builder.setMessage("Para utilizar essa função, aceite as permissões");
-        builder.setCancelable(false);
-        builder.setPositiveButton("Confirmar", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                getActivity().finish();
-            }
-        });
-        AlertDialog dialog = builder.create();
-        dialog.show();
 
     }
 }
--- app/src/main/java/com/example/testepicpic/model/Usuario.java
+++ app/src/main/java/com/example/testepicpic/model/Usuario.java
@@ -1,7 +1,15 @@
 package com.example.testepicpic.model;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
 import com.example.testepicpic.helper.Base64Custom;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 import com.google.firebase.database.DatabaseReference;
 import com.example.testepicpic.config.ConfigFirebase;
 import com.google.firebase.database.Exclude;
@@ -137,12 +145,38 @@ public class Usuario {
                 .child(this.idUser)
                 .setValue(this);
     }
-    public static String getIdentificadorUsuario() {
-        FirebaseAuth usuario = ConfigFirebase.getFirebaseAutenticacao();
-        String email = usuario.getCurrentUser().getEmail();
-        String identificadorUsuario = Base64Custom.codificarBase64(email);
 
-        return identificadorUsuario;
+  //PARA USAR NO PERFIL
+
+   public static FirebaseUser getUsuarioAtual(){
+        FirebaseAuth usuario = ConfigFirebase.getFirebaseAutenticacao();
+
+        return usuario.getCurrentUser();
+
+    }
+
+    public static void atualizarNomeUsuario (String nome){
+        try {
+
+            FirebaseUser usuariologado = getUsuarioAtual();
+
+            UserProfileChangeRequest profile = new UserProfileChangeRequest
+                    .Builder()
+                    .setDisplayName(nome)
+                    .build();
+            usuariologado.updateProfile(profile).addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    if(!task.isSuccessful()) {
+                        Log.d ("Perfil","Erro ao atualizar o nome no perfil");
+                    }
+
+                }
+            });
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
 }
