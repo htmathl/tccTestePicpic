873301fbf946ae8fc087a29f53e2ae224def29d2
Anna <annabserikyaku@gmail.com>
2020-11-08 18:59:37 -0300

    ta torto mano :(

---
+++ app/src/main/java/com/example/testepicpic/MyFirebaseMessagingService.java
@@ -0,0 +1,25 @@
+package com.example.testepicpic;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+
+        Log.i("notificacao", "notificacao recebida");
+
+
+    }
+
+    @Override
+    public void onNewToken(@NonNull String s) {
+        super.onNewToken(s);
+    }
+}
