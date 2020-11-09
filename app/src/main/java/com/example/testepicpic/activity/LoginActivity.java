7a42c3d43baccec07692026fe254c39a2b52f4b7
matheus <matheus.domingues3@etec.sp.gov.br>
2020-11-09 01:53:07 -0300

    14 horas :))

--- app/src/main/java/com/example/testepicpic/activity/LoginActivity.java
+++ app/src/main/java/com/example/testepicpic/activity/LoginActivity.java
@@ -23,7 +23,10 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.example.testepicpic.config.ConfigFirebase;
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
 import com.google.firebase.auth.FirebaseAuthInvalidUserException;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.Calendar;
 
@@ -92,6 +95,65 @@ public class LoginActivity extends AppCompatActivity {
                             Toast.makeText(LoginActivity.this, "Bem vindo, " + user.getEmail(), Toast.LENGTH_SHORT).show();
                             LocalBroadcastManager.getInstance(LoginActivity.this).sendBroadcast(new Intent("fecharTelaPrincipal"));
                             verificarUserLogado();
+
+                            DatabaseReference ref = ConfigFirebase.getFirebase();
+
+                            int pYear = Calendar.getInstance().get(Calendar.YEAR);
+                            int pMonth = (Calendar.getInstance().get(Calendar.MONTH)+1);
+                            int pDay = Calendar.getInstance().get(Calendar.DAY_OF_MONTH);
+
+                            String data = String.valueOf(pYear) + String.valueOf(pMonth) + String.valueOf(pDay);
+
+                            FirebaseAuth auth = ConfigFirebase.getFirebaseAutenticacao();
+
+                            String email = auth.getCurrentUser().getEmail();
+                            assert email != null;
+                            String currentId = Base64Custom.codificarBase64(email);
+
+                            DatabaseReference reference = ref.child("inserção")
+                                    .child(currentId)
+                                    .child("bem-estar");
+
+                            reference.addValueEventListener(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                                    for(DataSnapshot dataSnapshot : snapshot.getChildren()) {
+
+                                        DatabaseReference reference1 =  ref.child("inserção")
+                                                                        .child(currentId)
+                                                                        .child("bem-estar")
+                                                                        .child(dataSnapshot.getKey())
+                                                                        .child("Água");
+
+                                        reference1.addValueEventListener(new ValueEventListener() {
+                                            @Override
+                                            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                                                ref.child("inserção")
+                                                        .child(currentId)
+                                                        .child("bem-estar")
+                                                        .child(dataSnapshot.getKey())
+                                                        .child("Água")
+                                                        .setValue(snapshot.getValue());
+
+                                            }
+
+                                            @Override
+                                            public void onCancelled(@NonNull DatabaseError error) {
+
+                                            }
+                                        });
+
+                                    }
+
+                                }
+
+                                @Override
+                                public void onCancelled(@NonNull DatabaseError error) {
+
+                                }
+                            });
 
                         } else {
 
