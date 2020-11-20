b597ec1536ace06594066541c1dd1608c425e9e9
Anna <annabserikyaku@gmail.com>
2020-11-20 04:01:56 -0300

    so pra  não bugar

--- app/src/main/java/com/example/testepicpic/config/ConfigFirebase.java
+++ app/src/main/java/com/example/testepicpic/config/ConfigFirebase.java
@@ -1,8 +1,5 @@
 package com.example.testepicpic.config;
 
-import android.os.storage.StorageManager;
-
-import com.google.android.gms.auth.api.signin.internal.Storage;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
--- app/src/main/java/com/example/testepicpic/fragment/ConfigPerfilFragment.java
+++ app/src/main/java/com/example/testepicpic/fragment/ConfigPerfilFragment.java
@@ -1,9 +1,21 @@
 package com.example.testepicpic.fragment;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageItemInfo;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,11 +27,16 @@ import android.widget.TextView;
 import com.bumptech.glide.Glide;
 import com.example.testepicpic.R;
 import com.example.testepicpic.config.ConfigFirebase;
+import com.example.testepicpic.helper.Permissao;
 import com.example.testepicpic.model.Usuario;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.storage.StorageReference;
 
+import java.util.jar.Pack200;
+
 import de.hdodenhof.circleimageview.CircleImageView;
+
+import static android.app.Activity.RESULT_OK;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -35,9 +52,16 @@ public class ConfigPerfilFragment extends Fragment {
     private StorageReference storageReference;
     private String IdentificadorUsuario;
 
+
     private ImageButton imgbtnCamera, imgbtnGaleria;
 
-   // private StorageReference storageReference;
+    private String[] permissoesNecessarias = new String[]{
+            Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.CAMERA
+    };
+    private static int SELECAO_CAMERA = 100;
+    private static int SELECAO_GALERIA = 200;
+
+    // private StorageReference storageReference;
    // private String identificadorUsuario;
 
 
@@ -88,19 +112,23 @@ public class ConfigPerfilFragment extends Fragment {
 
         /*btnSalvarPerfil= view.findViewById(R.id.btnSalvarPerfil);
         txtAlterarFoto = view.findViewById(R.id.txtAlterarFoto);
-        edtNomeUser= view.findViewById(R.id.edtNomeUser);
+        edtNomeUser= view.findViewById(R.id.edtNomeUser);*/
 
-        storageReference= ConfigFirebase.getFirebaseStorage();*/
+        storageReference= ConfigFirebase.getFirebaseStorage();
+
+        Permissao.validarpermisoes(permissoesNecessarias, getActivity(),1);
 
         imgbtnCamera = view.findViewById(R.id.imgBtnCamera);
         imgbtnGaleria = view.findViewById(R.id.imgBtnGaleria);
+        circleImageViewPerfil = view.findViewById(R.id.circleImageViewFotoPerfil);
 
         imgbtnCamera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-
-
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if(intent.resolveActivity(getActivity().getPackageManager())!=null){
+                startActivityForResult(intent, SELECAO_CAMERA);}
             }
         });
 
@@ -108,10 +136,71 @@ public class ConfigPerfilFragment extends Fragment {
             @Override
             public void onClick(View v) {
 
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                if(intent.resolveActivity(getActivity().getPackageManager())!=null){
+                    startActivityForResult(intent,SELECAO_GALERIA);
+                }
             }
         });
 
-
         return view;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == RESULT_OK){
+            Bitmap imagem = null;
+            try{
+                switch (requestCode){
+                    case 100:
+                        imagem = (Bitmap) data.getExtras().get("data");
+                        break;
+                    case 200:
+                        Uri localImagemSelecionada = data.getData();
+                        imagem = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(),localImagemSelecionada);
+                        break;
+                }
+                if(imagem != null){
+                    circleImageViewPerfil.setImageBitmap(imagem);
+                    StorageReference imagemRef = storageReference
+                            .child("imagens")
+                            .child("perfil")
+                            .child("");
+                }
+
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode,permissions,grantResults);
+
+        for(int permissaoResultado : grantResults){
+            if(permissaoResultado== PackageManager.PERMISSION_DENIED){
+                alertaValidacaoPermissao();
+            }
+        }
+    }
+
+    public void alertaValidacaoPermissao(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Permissões Negadas");
+        builder.setMessage("Para utilizar o aplicativo é necessário aceitar as permissões");
+        builder.setCancelable(false);
+        builder.setPositiveButton("Confirmar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                getActivity().finish();
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
     }
 }
